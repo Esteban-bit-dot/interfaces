dffaafd10361409f3ae1613d0551cb3730d3a965
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-24 17:06:44 -0500

    Create Controlador_Vehiculo2.java

---
+++ Controlador_Vehiculo2.java
@@ -0,0 +1,11 @@
+package controlador;
+
+import Vista.Formulario_Registro_Vehiculo;
+
+public class proyectointerfaz {
+    
+public static void main(String[]args){
+    formulario_Registro_Vehiculo.objRegistro_Vehiculo new Registro_Vehiculo;
+    objRegistro_Vehiculo.setVisible(b:true);
+    }
+}
