b896a50e6908fe663b8eda939c5178ae91af4d18
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-24 16:58:25 -0500

    Create Modelo_Usuario.java

---
+++ Modelo_Usuario.java
@@ -0,0 +1,39 @@
+package Modelo;
+
+
+public class usuario {
+    
+    private String Nombre_usuario;
+    private String Correo_usuario;
+    private String Contraseña_usuario;
+
+    public void Vehiculo(){
+        this.Nombre_usuario;
+        this.Correo_usuario;
+        this.Contraseña_usuario;
+    }
+
+    public String getNombre_usuario() {
+        return Nombre_usuario;
+    }
+
+    public void setSerial_vehiculo(String Nombre_usuario) {
+        this.Nombre_usuario = Nombre_usuario;
+    }
+
+    public String getCorreo_usuario() {
+        return Correo_usuario;
+    }
+
+    public void setCorreo_usuario(String Correo_usuario) {
+        this.Correo_usuario = Correo_usuario;
+    }
+    
+    public String getContraseña_usuario(){
+        return Contraseña_usuario;
+    }
+
+    public void setContraseña_usuario(String Contraseña_usuario) {
+        this.Contraseña_usuario = Contraseña_usuario;
+    }
+}
