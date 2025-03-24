1744a93cd7254c8d06ecc80ee498b64941567a60
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-24 17:07:42 -0500

    Create Controlador_Usuario2.java

---
+++ Controlador_Usuario2.java
@@ -0,0 +1,12 @@
+package controlador;
+
+import Vista.Formulario_Registro_Usuario;
+
+public class proyecto_interfaz2 {
+    
+ 
+public static void main(String[]args){
+    formulario_Registro_Usuario.objRegistro_Usuario new Registro_Usuario;
+    objRegistro_Usuario.setVisible(b:true);
+    }
+}
