079632f758751bc2db9ce117bebf29f51948c521
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-24 17:08:35 -0500

    Create proyecto_interfaz.java

---
+++ proyecto_interfaz.java
@@ -0,0 +1,12 @@
+package proyecto_interfaz;
+
+import Controlador.Controlador;
+
+public class principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Controlador objControlador = new Controlador;
+    }
+    
+}
